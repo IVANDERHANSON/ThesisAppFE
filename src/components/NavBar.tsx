98e1858a789a2120f052310c539372c56abfeb26
hansonivander <hansonivander@gmail.com>
2024-11-22 22:53:49 +0700

    feat: setup react router, tailwind, flowbite, and navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import { Navbar } from "flowbite-react";
+
+export default function ({ image }: { image: string }) {
+    return (
+        <>
+            <Navbar fluid rounded>
+                <Navbar.Brand href="/">
+                    <img src={image} className="mr-3 h-6 sm:h-9" alt="Thesis App Logo" />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Thesis App</span>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Navbar.Link href="/">Home</Navbar.Link>
+                    <Navbar.Link href="/admin/dashboard">Admin Dashboard</Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
